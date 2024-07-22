20419911a4037fdc9a5b5d43cecd612a71724e72
Lawrence Tsang <pikalaw@gmail.com>
2024-07-21 22:26:15 -0400

    Reformat the output so that all experiments live in the same csv row

--- prompt_eval/__main__.py
+++ prompt_eval/__main__.py
@@ -1,29 +1,118 @@
 import asyncio
+import csv
 from devtools import debug
 from huggingface_hub import login
 from io import TextIOWrapper
 import logging
 import math
 import os
-from typing import Any, Awaitable, Callable, Iterable, Iterator
+from pydantic import BaseModel
+from typing import cast, Iterable, Iterator, TypeVar
 from .dataset_loader import load_samples, Sample
 from .eval_baseline import eval_baseline
 from .eval_1_prompt_reflection import eval_1_prompt_reflection
 from .eval_N_prompts_reflection import eval_N_prompts_reflection
 
 
-async def eval_and_log(
+T = TypeVar("T")
+
+
+class Experiment(BaseModel):
+    # Data from the eval dataset.
+    question: str
+    human_answer: str
+
+    # Output from the baseline model.
+    model_baseline_answer: str | None = None
+    grade_baseline: int | None = None
+
+    # Output from the 1-prompt reflection model.
+    model_1_prompt_answer: str | None = None
+    grade_1_prompt: int | None = None
+
+    # Output from the N-prompts reflection model.
+    model_N_prompts_initial_answer: str | None = None
+    model_N_prompts_reflection: str | None = None
+    model_N_prompts_final_answer: str | None = None
+    grade_N_prompts: int | None = None
+
+
+async def eval_all_experiments(
     model: str,
-    eval_func: Callable[[str, Sample], Awaitable[Any]],
     sample: Sample,
-    output: TextIOWrapper,
-) -> None:
+) -> Experiment:
+    experiment = Experiment(
+        question=sample.question,
+        human_answer=sample.answer,
+    )
+
     try:
-        experiment = await eval_func(model, sample)
-        output.write(str(debug.format(experiment)))
-        output.flush()
+        baseline_experiment = await eval_baseline(model, sample)
+
+        experiment.model_baseline_answer = baseline_experiment.model_answer
+        experiment.grade_baseline = baseline_experiment.grade
     except Exception as e:
         logging.exception(debug.format(sample))
+
+    try:
+        one_prompt_experiment = await eval_1_prompt_reflection(model, sample)
+
+        experiment.model_1_prompt_answer = one_prompt_experiment.model_answer
+        experiment.grade_1_prompt = one_prompt_experiment.grade
+    except Exception as e:
+        logging.exception(debug.format(sample))
+
+    try:
+        n_prompt_experiment = await eval_N_prompts_reflection(model, sample)
+
+        experiment.model_N_prompts_initial_answer = (
+            n_prompt_experiment.initial_model_answer
+        )
+        experiment.model_N_prompts_reflection = n_prompt_experiment.reflection
+        experiment.model_N_prompts_final_answer = n_prompt_experiment.final_model_answer
+        experiment.grade_N_prompts = n_prompt_experiment.grade
+    except Exception as e:
+        logging.exception(debug.format(sample))
+
+    return experiment
+
+
+def escape_string(s: T) -> T:
+    """Escape all \n characters."""
+    if s is None:
+        return None
+    assert isinstance(s, str)
+    return cast(T, s.replace("\n", "\\n"))
+
+
+def format_experiment(experiment: Experiment) -> Experiment:
+    """For each string fields, escape all \n characters."""
+    return Experiment(
+        question=escape_string(experiment.question),
+        human_answer=escape_string(experiment.human_answer),
+        model_baseline_answer=escape_string(experiment.model_baseline_answer),
+        grade_baseline=experiment.grade_baseline,
+        model_1_prompt_answer=escape_string(experiment.model_1_prompt_answer),
+        grade_1_prompt=experiment.grade_1_prompt,
+        model_N_prompts_initial_answer=escape_string(
+            experiment.model_N_prompts_initial_answer
+        ),
+        model_N_prompts_reflection=escape_string(experiment.model_N_prompts_reflection),
+        model_N_prompts_final_answer=escape_string(
+            experiment.model_N_prompts_final_answer
+        ),
+        grade_N_prompts=experiment.grade_N_prompts,
+    )
+
+
+async def eval_and_log(
+    model: str,
+    sample: Sample,
+    writer: csv.DictWriter,
+) -> None:
+    experiment = await eval_all_experiments(model, sample)
+    formatted_experiment = format_experiment(experiment)
+    writer.writerow(formatted_experiment.model_dump())
 
 
 def batch_samples(samples: Iterable[Sample], batch_size: int) -> Iterator[list[Sample]]:
@@ -38,7 +127,6 @@ def batch_samples(samples: Iterable[Sample], batch_size: int) -> Iterator[list[S
 async def run_eval(
     *,
     model: str,
-    eval_func: Callable[[str, Sample], Awaitable[Any]],
     samples: Iterable[Sample],
     output_filename: str,
     batch_size: int = 10,
@@ -48,9 +136,16 @@ async def run_eval(
     done = 0
     bad = 0
     with open(output_filename, "w") as output:
+        writer = csv.DictWriter(
+            output,
+            fieldnames=Experiment.model_fields.keys(),
+            quoting=csv.QUOTE_ALL,
+        )
+        writer.writeheader()
+
         for batch in batch_samples(samples, batch_size=batch_size):
             results = await asyncio.gather(
-                *[eval_and_log(model, eval_func, sample, output) for sample in batch],
+                *[eval_and_log(model, sample, writer) for sample in batch],
                 return_exceptions=True,
             )
 
@@ -83,25 +178,8 @@ async def main() -> None:
 
     await run_eval(
         model=model,
-        eval_func=eval_baseline,
         samples=samples,
-        output_filename="baseline_output.log",
-        batch_size=batch_size,
-        limit=limit,
-    )
-    await run_eval(
-        model=model,
-        eval_func=eval_1_prompt_reflection,
-        samples=samples,
-        output_filename="1_prompts_output.log",
-        batch_size=batch_size,
-        limit=limit,
-    )
-    await run_eval(
-        model=model,
-        eval_func=eval_N_prompts_reflection,
-        samples=samples,
-        output_filename="N_prompts_output.log",
+        output_filename="eval_result.csv",
         batch_size=batch_size,
         limit=limit,
     )
